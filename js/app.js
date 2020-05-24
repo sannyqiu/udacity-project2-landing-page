8afd4999c19935d81eeb5c45161fb0964ef93491
BGLCROP/sqiu <sqiu@bglcorp.com.au>
2020-05-24 16:14:55 +1000

    Project2 complete

--- js/app.js
+++ js/app.js
@@ -15,8 +15,10 @@
 
 /**
  * Define Global Variables
- *
  */
+
+const sections = document.getElementsByTagName("section");
+const nav = document.getElementsByClassName("navbar__menu")[0];
 
 /**
  * End Global Variables
@@ -31,86 +33,63 @@
  */
 
 // build the nav
-// buildNav = () => {
-//   const navItemArray = ["Section 1", "Section 2", "Section 3", "Section 4"];
-
-//   const navList = document.querySelector("ul");
-//   // navList.className = "navbar__menu";
-//   for (const navItem of navItemArray) {
-//     const listItem = document.createElement("li");
-//     listItem.className = "navbar__menu";
-//     listItem.innerHTML = `<a href="#" class="menu__link">${navItem}</a>`;
-//     navList.appendChild(listItem);
-//   }
-// };
-
-// buildNav();
 
 buildNav = () => {
   const navBarList = document.querySelector("ul");
   navBarList.id = "navbar__list";
-  // const sections = document.querySelectorAll("section");
-  const sections = document.getElementsByTagName("section");
 
   for (const section of sections) {
     const listItem = document.createElement("li");
-    // listItem.className = "navbar__menu";
     listItem.innerHTML = `<a href="#${section.id}" class="menu__link" data-id="${section.id}">${section.dataset.nav}</a>`;
     navBarList.appendChild(listItem);
-    // console.log(listItem);
   }
 };
-
-buildNav();
 
 // Add class 'active' to section when near top of viewport
 
 makeActive = () => {
-  const sections = document.querySelectorAll("section");
-  // console.log(sections);
-  // const sections = document.getElementsByTagName("section");
-
   for (const section of sections) {
     const box = section.getBoundingClientRect();
     const id = document.querySelectorAll(`[data-id="${section.id}"]`)[0];
-    // You can play with the values in the "if" condition to further make it more accurate.
     if (box.top <= 300 && box.bottom >= 300) {
       section.classList.add("your-active-class");
-
-      console.log(id);
       id.classList.add("active");
-      // const id = section.getAttribute("id");
-      // document.querySelector(`.${idd}`).classList.add("active");
-
-      // Apply active state on the current section and the corresponding Nav link.
     } else {
       section.classList.remove("your-active-class");
       console.log(id);
       id.classList.remove("active");
-      // const id = document.querySelectorAll(
-      //   `[data-id="${section.dataset.nav}"]`
-      // );
-      // document.getElementById(`${id}`).classList.remove("active");
-      // Remove active state from other section and corresponding Nav link.
     }
-    // console.log(section);
   }
 };
 
+// Hide nav bar when not scrolling
+let timer = null;
+hideNav = () => {
+  if (timer !== null) {
+    clearTimeout(timer);
+  }
+  timer = setTimeout(function () {
+    nav.style.display = "none";
+  }, 500);
+};
+
+// Build menu
+buildNav();
+
+// Set sections as active
 document.addEventListener("scroll", function () {
   makeActive();
 });
 
-// Scroll to anchor ID using scrollTO event
+// Hide and show nav bar
+document.addEventListener(
+  "scroll",
+  function () {
+    hideNav();
+  },
+  false
+);
 
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-// Build menu
-
-// Scroll to section on link click
-
-// Set sections as active
+document.addEventListener("scroll", function () {
+  nav.style.display = "block";
+});
